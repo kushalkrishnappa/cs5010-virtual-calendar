3034a4d40c49f57ef0fc2ca288c99b956e8bcdcb
Kushal Krishnappa <kushalkrishnappa333@gmail.com>
2025-03-02 18:28:40 -0500

    Assignment4: Implemented EventDTO - Created a buidler class - Updated interface - tests for the EventDTO class

--- src/main/java/dto/EventDTO.java
+++ src/main/java/dto/EventDTO.java
@@ -2,18 +2,181 @@ package dto;
 
 import java.time.LocalDateTime;
 
+/**
+ * EventDTO class that implements IEventDTO and store information about an event.
+ */
+public class EventDTO implements IEventDTO {
 
-public interface EventDTO {
+  private final String subject;
+  private final LocalDateTime startTime;
+  private final LocalDateTime endTime;
+  private final String description;
+  private final String location;
+  private final Boolean isPublic;
 
-  String getSubject();
+  /**
+   * Private constructor for EventDTO. The object is created using the EventDTOBuilder class.
+   *
+   * @param subject The subject of the event
+   * @param startTime The start time of the event
+   * @param endTime The end time of the event
+   * @param description The description of the event
+   * @param location The location of the event
+   * @param isPublic Whether the event is public or not
+   */
+  private EventDTO(
+      String subject,
+      LocalDateTime startTime,
+      LocalDateTime endTime,
+      String description,
+      String location,
+      Boolean isPublic) {
+    this.subject = subject;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.description = description;
+    this.location = location;
+    this.isPublic = isPublic;
+  }
 
-  LocalDateTime getStart();
+  /**
+   * Returns a new EventDTOBuilder object. It is used to build an EventDTO object with the required
+   * attributes recursively.
+   *
+   * @return eventDTOBuilder object
+   */
+  public static EventDTOBuilder getBuilder() {
+    return new EventDTOBuilder();
+  }
 
-  LocalDateTime getEnd();
+  /**
+   * EventDTOBuilder class that builds an EventDTO object.
+   */
+  public static class EventDTOBuilder {
 
-  String getDescription();
+    private String subject;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String description;
+    private String location;
+    private Boolean isPublic;
 
-  String getLocation();
+    /**
+     * Private empty constructor for EventDTOBuilder.
+     */
+    private EventDTOBuilder() {
+    }
 
-  boolean isPublic();
+    /**
+     * Sets the subject of the event.
+     *
+     * @param subject The subject of the event
+     * @return EventDTOBuilder object
+     */
+    public EventDTOBuilder setSubject(String subject) {
+      this.subject = subject;
+      return this;
+    }
+
+    /**
+     * Sets the start time of the event.
+     *
+     * @param startTime The start time of the event
+     * @return EventDTOBuilder object
+     */
+    public EventDTOBuilder setStartTime(LocalDateTime startTime) {
+      this.startTime = startTime;
+      return this;
+    }
+
+    /**
+     * Sets the end time of the event.
+     *
+     * @param endTime The end time of the event
+     * @return EventDTOBuilder object
+     */
+    public EventDTOBuilder setEndTime(LocalDateTime endTime) {
+      this.endTime = endTime;
+      return this;
+    }
+
+    /**
+     * Sets the description of the event.
+     *
+     * @param description The description of the event
+     * @return EventDTOBuilder object
+     */
+    public EventDTOBuilder setDescription(String description) {
+      this.description = description;
+      return this;
+    }
+
+    /**
+     * Sets the location of the event.
+     *
+     * @param location The location of the event
+     * @return EventDTOBuilder object
+     */
+    public EventDTOBuilder setLocation(String location) {
+      this.location = location;
+      return this;
+    }
+
+    /**
+     * Sets whether the event is public or not.
+     *
+     * @param isPublic Whether the event is public or not
+     * @return EventDTOBuilder object
+     */
+    public EventDTOBuilder setIsPublic(boolean isPublic) {
+      this.isPublic = isPublic;
+      return this;
+    }
+
+    /**
+     * Builds an EventDTO object with attributes from EventDTOBuilder class.
+     *
+     * @return EventDTO object
+     */
+    public EventDTO build() {
+      return new EventDTO(
+          this.subject,
+          this.startTime,
+          this.endTime,
+          this.description,
+          this.location,
+          this.isPublic
+      );
+    }
+  }
+
+  @Override
+  public String getSubject() {
+    return this.subject;
+  }
+
+  @Override
+  public LocalDateTime getStartTime() {
+    return this.startTime;
+  }
+
+  @Override
+  public LocalDateTime getEndTime() {
+    return this.endTime;
+  }
+
+  @Override
+  public String getDescription() {
+    return this.description;
+  }
+
+  @Override
+  public String getLocation() {
+    return this.location;
+  }
+
+  @Override
+  public Boolean isPublic() {
+    return this.isPublic;
+  }
 }
