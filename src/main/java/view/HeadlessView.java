46b63228b2da5b2955982d3a19d8b3b169c5b65c
Kushal Krishnappa <kushalkrishnappa333@gmail.com>
2025-03-03 15:43:05 -0500

    Assignment4: Added IView interface empty implementations - cli view - headless view

---
+++ src/main/java/view/HeadlessView.java
@@ -0,0 +1,21 @@
+package view;
+
+import controller.IController;
+
+public class HeadlessView implements IView {
+
+  @Override
+  public void displayOutput(String output) {
+    // TODO: Implement this method
+  }
+
+  @Override
+  public void displayError(String error) {
+    // TODO: Implement this method
+  }
+
+  @Override
+  public void addController(IController controller) {
+    // TODO: Implement this method
+  }
+}
