66ff2001fc035f53e1ddb784b93d7b2a645bbb8e
Kushal Krishnappa <kushalkrishnappa333@gmail.com>
2025-03-08 13:40:45 -0500

    Assignment4: Removed previous conflict implementation

--- src/main/java/model/CalendarModel.java
+++ src/main/java/model/CalendarModel.java
@@ -30,9 +30,7 @@ public class CalendarModel implements IModel {
       throws EventConflictException, IllegalArgumentException {
     // if autodecline is set, check for conflicts and throw exception if there is a conflict
     if (autoDecline) {
-      if (doesEventConflict(eventDTO)) {
-        throw new EventConflictException("Event conflicts with existing event");
-      }
+      // TODO: check for conflict
     }
     // check what kind of event to create
     if (eventDTO.getStartTime() != null && eventDTO.getEndTime() != null) {
@@ -42,44 +40,6 @@ public class CalendarModel implements IModel {
     } else {
       throw new IllegalArgumentException("Invalid event");
     }
-  }
-
-  private boolean doesEventConflict(EventDTO newEvent) {
-    // get all the events on the specific date
-    List<EventDTO> events = eventRepository.getEventsOnDate(newEvent.getStartTime().toLocalDate());
-    // iterate the events and check if the user is busy
-    for (EventDTO storedEvent : events) {
-      if (// condition 1: if the storedEvent is all day event return true
-          isALlDayEvent(storedEvent)
-          // condition 2: if the new event start time overlaps with stored event return true
-          || doesNewEventStartTimeOverlap(newEvent, storedEvent)
-          // condition 3: if the new event end time overlaps with stored event return true
-          || doesNewEventEndTimeOverlap(newEvent, storedEvent)
-          // condition 4: if the new event mask with stored event return true
-          || doesNewEventMask(newEvent, storedEvent)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private static boolean isALlDayEvent(EventDTO storedEvent) {
-    return Objects.isNull(storedEvent.getEndTime());
-  }
-
-  private static boolean doesNewEventStartTimeOverlap(EventDTO newEvent, EventDTO storedEvent) {
-    return newEvent.getStartTime().isBefore(storedEvent.getEndTime()) && newEvent.getEndTime()
-        .isAfter(storedEvent.getStartTime());
-  }
-
-  private static boolean doesNewEventEndTimeOverlap(EventDTO newEvent, EventDTO storedEvent) {
-    return newEvent.getEndTime().isAfter(storedEvent.getStartTime()) && newEvent.getStartTime()
-        .isBefore(storedEvent.getEndTime());
-  }
-
-  private static boolean doesNewEventMask(EventDTO newEvent, EventDTO storedEvent) {
-    return newEvent.getStartTime().isBefore(storedEvent.getStartTime()) && newEvent.getEndTime()
-        .isAfter(storedEvent.getEndTime());
   }
 
   @Override
